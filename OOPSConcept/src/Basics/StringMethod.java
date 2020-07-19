70f56a276cd5fe257b08b55a407ac6ab79c7f1ec
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-19 20:37:10 +0100

    IsEmpty, typecasting for String

--- OOPSConcept/src/Basics/StringMethod.java
+++ OOPSConcept/src/Basics/StringMethod.java
@@ -10,5 +10,15 @@ public class StringMethod {
 		else {
 			System.out.println("Name :" +name.trim());
 		}
+		
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
+		int a =10, b=20;
+		String s1 = String.valueOf(a);
+		String s2 = String.valueOf(b);
+		System.out.println(a+b);
+		System.out.println(s1+s2);
+		char[] c = name.toCharArray();
+		System.out.println(c);
 	}
 }
