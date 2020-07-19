aa45df06d31c986bd0c22981f643a2f8d95a676c
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-19 19:40:49 +0100

    Inner Class

--- OOPSConcept/src/Basics/InnerClassDemo.java
+++ OOPSConcept/src/Basics/InnerClassDemo.java
@@ -18,8 +18,8 @@ public class InnerClassDemo {
 		System.out.println(inn.value2);
 		System.out.println("Inner method "+inn.InnerMethod());
 		
-//		for static inner class there is no need to use outer 
-//		class object to create object of inner class
+//		for static inner class there is no need to use outer class object
+//		Outer class object to create object of inner class
 		InnerClassDemo.InnerStatic ins = new InnerClassDemo.InnerStatic();
 		System.out.println(ins.value3);
 	}
