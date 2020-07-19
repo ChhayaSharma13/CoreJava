186dbde2ed0ce742847e785c101795d9399636b0
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-19 19:39:54 +0100

    Printf Function

--- OOPSConcept/src/Basics/PrintfStringSpace.java
+++ OOPSConcept/src/Basics/PrintfStringSpace.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 public class PrintfStringSpace {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		/*
-		 * for(int i=0;i<3;i++) { int x = sc.nextInt(); String s = sc.next(); }
-		 */
+		
               String s1=sc.next();
               int x=sc.nextInt();
               String s2=sc.next();
               int x2=sc.nextInt();
               String s3=sc.next();
               int x3=sc.nextInt();
-		/*
-		 * String s2 = sc.next(); int x2 = sc.nextInt(); String s3 = sc.next(); int x3 =
-		 * sc.nextInt();
-		 */
-        
+		
 		  System.out.printf("%-15s %d%n", s1, x);
 		  System.out.printf("%-15s %d%n", s2, x2);
 		  System.out.printf("%-15s %d%n", s3, x3);
