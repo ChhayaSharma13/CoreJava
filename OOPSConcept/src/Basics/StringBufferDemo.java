dec8dfdc9e5dbe85d80603f83748cf63393cab62
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-20 00:13:19 +0100

    String Buffer Methods (capacity, equals,substring,trim)

--- OOPSConcept/src/Basics/StringBufferDemo.java
+++ OOPSConcept/src/Basics/StringBufferDemo.java
@@ -15,13 +15,33 @@ public class StringBufferDemo {
 		System.out.println(sb.capacity());
 		
 		System.out.println(sb.length());
-		//capacity : How much characters an object store
+//		capacity : How much characters an object store
 //		while length  gives no. of characters stored
 		System.out.println(sb.delete(2, 5));
 		System.out.println(sb);
 		System.out.println(sb.equals(sb2));
 		System.out.println(sb.equals(s3));
-//		String buffer doesn't override method of String class
+//		String buffer doesn't override equals() method of String class
+		System.out.println(sb.indexOf("ll"));
+		System.out.println(sb.insert(3, "replace"));
+		System.out.println(sb.replace(2,8,"g"));
+		
+//		subString 
+		System.out.println(sb.substring(8));
+		sb.toString();
+		
+		System.out.println(sb2+ "Hello");
+		
+		sb.setLength(6);
+		System.out.println(sb);
+		
+//		to delete unnecessary space use trim() method
+		sb2.ensureCapacity(100);
+		sb2.append("Hii");
+		sb2.trimToSize();
+		System.out.println(sb2);
+		System.out.println(sb2.capacity());
+		
 		
 		
 	
