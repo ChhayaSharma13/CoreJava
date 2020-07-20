2d2fc79b5af9d3d8df9fe76c3c03511e6bbaf76c
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-20 14:28:54 +0100

    JDBC Demo

---
+++ OOPSConcept/src/jdbcJava/DemoClass.java
@@ -0,0 +1,17 @@
+package jdbcJava;
+
+public class DemoClass {
+//	1.import  --> java.sql
+//	2.load and register the driver -->com.mysql.jdbc.Driver
+//	3.Create a connection
+//	4.Create a statement
+//	5.execute the query
+//	6.process the results
+//	7.Close
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
