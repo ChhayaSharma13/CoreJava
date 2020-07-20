ee3d6fd3822222fbcc099d90fe71b3a92b743a9e
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-20 14:42:38 +0100

    ArrayDemo

---
+++ OOPSConcept/src/arrayJava/ArrayDemo.java
@@ -0,0 +1,21 @@
+package arrayJava;
+
+public class ArrayDemo {
+	static int[] a = new int[3];
+	int[] b = new int[3];
+	
+	public static void displayArray() {
+		for(int i=1; i< a.length;i++) {
+			System.out.println(a[i]);
+		}
+	}
+	public static void main(String[] args) {
+	
+		for(int i=1; i< a.length;i++) {
+			a[i] = 2 *i;
+		}	
+		displayArray();
+		
+	}
+
+}
