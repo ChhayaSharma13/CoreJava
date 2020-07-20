0d41c65bbdae31bf703c256d07f2c43776351aa7
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-20 18:36:44 +0100

    1D Array Declaration, Retrieval

--- OOPSConcept/src/arrayJava/ArrayDemo.java
+++ OOPSConcept/src/arrayJava/ArrayDemo.java
@@ -2,17 +2,20 @@ package arrayJava;
 
 public class ArrayDemo {
 	static int[] a = new int[10];
-	int[] b = new int[10];
+	
 	
 	public static void displayArray() {
-		for(int i=1; i< a.length;i++) {
-			System.out.println(a[i]);
+		for(int i=1; i<= a.length;i++) {
+			System.out.println(a[i-1]);
 		}
 	}
 	public static void main(String[] args) {
-	
-		for(int i=1; i< a.length;i++) {
-			a[i] = 2 *(i);
+		int[] b = new int[] {1,2,5};
+		for(int i=1;i<=b.length;i++) {
+			System.out.println(b[i-1]);
+		}
+		for(int i=1; i<= a.length;i++) {
+			a[i-1] = 2 *(i);
 		}	
 		displayArray();
 		
