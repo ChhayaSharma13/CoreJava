3573063f0d781a1fe4e77fa3f8b2819ad400167e
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-21 14:51:16 +0100

    3D aaray

--- OOPSConcept/src/arrayJava/ThreeDimensionalArray.java
+++ OOPSConcept/src/arrayJava/ThreeDimensionalArray.java
@@ -1,9 +1,18 @@
 package arrayJava;
 
 public class ThreeDimensionalArray {
-
+	static int[][][] a = {{{3},{4},{6},{67},{89},{56}},{{34},{45}}};
+	public static void displayArray() {
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a[i].length;j++) {
+				for(int k=0;k<a[i][j].length;k++) {
+					System.out.println(a[i][j][k]);
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
-		
+		displayArray();
 
 	}
 
