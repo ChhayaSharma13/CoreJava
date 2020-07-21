5a153f625302a6bca8b0dd6b0d920006af2acb90
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-21 12:22:27 +0100

    2D array

--- OOPSConcept/src/arrayJava/TwoDimensionalArray.java
+++ OOPSConcept/src/arrayJava/TwoDimensionalArray.java
@@ -1,12 +1,19 @@
 package arrayJava;
 
 public class TwoDimensionalArray {
-
+	static int[][] a = {{10,20,30},{1,2}};
+	public static void displayarray() {
+		for(int i=0;i<a.length;i++) {
+			for(int j=0; j<a[i].length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+		}
+	}
 	public static void main(String[] args) {
-		int[][] a = {{10,20,30},{1,2}};
 		System.out.println(a[1][1]);
 		System.out.println(a[0].length);
 		System.out.println(a.length);
+		displayarray();
 	}
 
 }
