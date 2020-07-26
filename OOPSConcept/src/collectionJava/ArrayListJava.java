7f0a5014ea4d248829f0faf094977130d2fafd8c
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-26 23:15:57 +0100

    ArrayList - Add Items

---
+++ OOPSConcept/src/collectionJava/ArrayListJava.java
@@ -0,0 +1,21 @@
+package collectionJava;
+
+import java.util.ArrayList;
+
+public class ArrayListJava {
+	
+	public static void main(String[] args) {
+		ArrayList<String> days = new ArrayList<String>();
+		days.add("Monday");
+		days.add("Tuesday");
+		days.add("Wednesday");
+		days.add("Thursday");
+		days.add("Wednesday");
+		days.add("Friday");
+		days.add("Saturday");
+		days.add("Sunday");
+		System.out.println(days);
+		
+	}
+
+}
