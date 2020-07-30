91ce40a5f8b2564c20cb94538f0455cd38f05519
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-07-30 17:35:06 +0100

    FileWriter

--- OOPSConcept/src/fileHandling/WriteToFileDemo.java
+++ OOPSConcept/src/fileHandling/WriteToFileDemo.java
@@ -7,7 +7,7 @@ public class WriteToFileDemo {
 	public static void main(String[] args) {
 		try {
 			FileWriter fWrite = new FileWriter("file1.txt");
-			fWrite.write("Files in Java is fun topic to practice and use");
+			fWrite.write("Files in Java is fun topic to practice and use.\n Now, I am appending this String to  see if it is going to write or do changes");
 			fWrite.close();
 			System.out.println("Successfully wrote to File!");
 		}catch(IOException e) {
