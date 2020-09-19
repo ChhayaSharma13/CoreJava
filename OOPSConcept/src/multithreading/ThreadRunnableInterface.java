c8f2c30096edbe3270b9cd8daa438a2c938c815c
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-19 17:46:01 +0100

    Thread - Runnable Interface

--- OOPSConcept/src/multithreading/ThreadRunnableInterface.java
+++ OOPSConcept/src/multithreading/ThreadRunnableInterface.java
@@ -1,10 +1,10 @@
 package multithreading;
 class Hey implements Runnable{
-
 	@Override
 	public void run() {
 		for(int i=0;i<=5;i++) {
 			System.out.println("Hey");
+			try {Thread.sleep(1000);}catch(Exception ex) {};
 		}
 	}	
 }
@@ -14,11 +14,20 @@ class  Namaste implements Runnable{
 	public void run() {
 		for(int i=0;i<=5;i++) {
 			System.out.println("Namaste");
+			try {Thread.sleep(1000);}catch(Exception ex) {};
 		}
 	}	
 }
 public class ThreadRunnableInterface{
 	public static void main(String[] args) {
+		Runnable h1 = new Hey();
+		Runnable n = new Namaste();
+//		Provide objects of Runnable as argument to object of Thread
+		Thread t1 = new Thread(h1);
+		Thread t2 = new Thread(n);
 		
+		t1.start();
+		try {Thread.sleep(10);}catch(Exception ex) {};
+		t2.start();
 	}
 }
