c5d39cc68efb5f4821c1854b4d9c2791ab46be09
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-19 20:04:17 +0100

    Set thread name at declaration

--- OOPSConcept/src/multithreading/ThreadPriority.java
+++ OOPSConcept/src/multithreading/ThreadPriority.java
@@ -13,10 +13,11 @@ public class ThreadPriority {
 				System.out.println("Namaste");
 				try {Thread.sleep(1000);}catch(Exception ex) {};
 			}
-		});
+		}, "Namaste Thread");
 		t1.start();
 		t1.setName("Hello Thread");
 		System.out.println(t1.getName());
+		System.out.println("2nd Thread: "+t2.getName());
 		try {Thread.sleep(100);}catch(Exception ex) {};
 		t2.start();
 		System.out.println("Bye");
