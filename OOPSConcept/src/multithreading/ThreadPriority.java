6b121b7a60281048d110ce7485643043404d0531
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-19 20:10:23 +0100

    Setting Thread Priority

--- OOPSConcept/src/multithreading/ThreadPriority.java
+++ OOPSConcept/src/multithreading/ThreadPriority.java
@@ -13,13 +13,24 @@ public class ThreadPriority {
 				System.out.println("Namaste");
 				try {Thread.sleep(1000);}catch(Exception ex) {};
 			}
-		}, "Namaste Thread");
+		});
 		t1.start();
-		t1.setName("Hello Thread");
-		System.out.println(t1.getName());
-		System.out.println("2nd Thread: "+t2.getName());
+//		t1.setName("Hello Thread");
+//		System.out.println(t1.getName());
+//		System.out.println("2nd Thread: "+t2.getName());
 		try {Thread.sleep(100);}catch(Exception ex) {};
 		t2.start();
+		
+//		Thread Priority ranges from lowest to highest as 1 to 10 respectively
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t2.setPriority(Thread.MAX_PRIORITY);
+		
+		System.out.println(t1.getPriority());
+		System.out.println(t2.getPriority());
+		
+		
+		t1.join();
+		t2.join();
 		System.out.println("Bye");
 	}
 }
