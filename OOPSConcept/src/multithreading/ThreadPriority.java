0f7883726a8bdcb5e8f93cc8a2adc3560161c6d3
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-19 20:02:28 +0100

    Get and Set Thread Name

---
+++ OOPSConcept/src/multithreading/ThreadPriority.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class ThreadPriority {
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(() ->{
+			for(int i=0;i<=5;i++) {
+				System.out.println("Hello");
+				try {Thread.sleep(1000);}catch(Exception ex) {};
+			}
+		});
+		Thread t2 = new Thread(() ->{
+			for(int i=0;i<=5;i++) {
+				System.out.println("Namaste");
+				try {Thread.sleep(1000);}catch(Exception ex) {};
+			}
+		});
+		t1.start();
+		t1.setName("Hello Thread");
+		System.out.println(t1.getName());
+		try {Thread.sleep(100);}catch(Exception ex) {};
+		t2.start();
+		System.out.println("Bye");
+	}
+}
