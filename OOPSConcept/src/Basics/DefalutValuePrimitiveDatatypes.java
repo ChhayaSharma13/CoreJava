fd2d0a4310d74a57755fc4643b2fb4f5833f18ab
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-19 20:24:01 +0100

    Default value of Primitive Datatypes

---
+++ OOPSConcept/src/Basics/DefalutValuePrimitiveDatatypes.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class DefalutValuePrimitiveDatatypes {
+	int count;
+	String name;
+	float f;
+	boolean result;
+	char c1;
+	double d;
+	byte b;
+	public static void main(String[] args) {
+		DefalutValuePrimitiveDatatypes c = new DefalutValuePrimitiveDatatypes();
+		System.out.println("Count: "+ c.count);
+		System.out.println(c.name);
+		System.out.println(c.f);
+		System.out.println(c.result);
+		System.out.println(c.c1);
+		System.out.println(c.d);
+		System.out.println(c.b);
+	}
+}
