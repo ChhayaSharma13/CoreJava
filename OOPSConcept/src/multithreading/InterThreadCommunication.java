d751c50608a04f875a79b0d090426089fb7edf5a
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-20 23:02:41 +0100

    Notify and wait

--- OOPSConcept/src/multithreading/InterThreadCommunication.java
+++ OOPSConcept/src/multithreading/InterThreadCommunication.java
@@ -1,12 +1,29 @@
 package multithreading;
 class A{
 	int num;
-	public void put(int num) {
+	boolean valueSet = false;
+//	Thread.sleep is  used when want to sleep the thread for some specific time and it will start to execute again automatically
+//	Wait() needs some thread to notify it for further execution 
+//	to avoid any conflicts between threads use Synchronized
+//	it is responsibility of both the threads to individually notify other thread that it can execute now
+//	one thread will wait other will notify
+//	*********wait() and notify() belong to object class********
+	public synchronized void put(int num) {
+		while(valueSet) {
+			try {wait();}catch(Exception ex) {};
+		}
 		System.out.println("Put: "+num);
 		this.num = num;
+		valueSet = true;
+		notify();
 	}
-	public void get() {
+	public synchronized void get() {
+		while(!valueSet) {
+			try {wait();}catch(Exception e) {};
+		}
 		System.out.println("Get: " +num);
+		valueSet =false;
+		notify();
 	}
 }
 class Producer implements Runnable{
@@ -32,7 +49,7 @@ class Consumer implements Runnable{
 	public void run() {
 		while(true) {
 			a.get();
-			try {Thread.sleep(1000);}catch(Exception e) {};
+			try {Thread.sleep(2000);}catch(Exception e) {};
 		}
 	}
 	public Consumer(A a) {
