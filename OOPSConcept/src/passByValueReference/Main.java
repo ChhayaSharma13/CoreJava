5c70506153d645f32900dfa40e0f732f5d2113f4
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-21 23:41:06 +0100

    Pass by Reference

--- OOPSConcept/src/passByValueReference/Main.java
+++ OOPSConcept/src/passByValueReference/Main.java
@@ -8,12 +8,30 @@ public class Main {
 		a = a+5;
 		System.out.println("a: "+ a);
 	}
+//	Pass by reference example
+	static void displayArray(int[] a) {
+		System.out.println("\n Inside display array method:");
+		a[0]=0; a[1]=0; a[2]=0;
+		for(int i=0;i<3;i++) {
+			System.out.println(a[i]+" ");
+		}
+	}
 	public static void main(String[] args) {
-		int a=5;
-		System.out.println("Before the function a: "+a);
-		displayPrimitive(a);
-		System.out.println("After the function a :" +a);
-		
+//		pass by  value
+//		int a=5;
+//		System.out.println("Before the function a: "+a);
+//		displayPrimitive(a);
+//		System.out.println("After the function a :" +a);
+		int arr[] = {2,2,3};
+		System.out.println("Before the function");
+		for(int i=0;i<3;i++) {
+			System.out.print(arr[i]+ " ");
+		}
+		displayArray(arr);
+		System.out.println("\n After the function");
+		for(int i=0;i<3;i++) {
+			System.out.print(arr[i]+ " ");
+		}
 	}
 
 }
